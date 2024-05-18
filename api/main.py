c8abee6b80d2413db85166c27432380f451a55fd
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-05-18 18:56:02 +0300

    upload

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "secret"
 
 @app.route("/")
 def index():
@@ -14,6 +15,12 @@ def pricing():
 def about():
     return render_template("about.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/workbook")
 def workbook():
-    return render_template("workboook.html")
+    if session.get("username", False):
+        return render_template("workboook.html")
+    return redirect("/")
